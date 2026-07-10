ed4f20b4a893b41b09567931d5992b5d1a041dd1
Sumiya Mohamed <sumiya12m@gmail.com>
2026-07-10 11:47:27 +0100

    Fixes to prov-library-app.sh

--- prov-library-app.sh
+++ prov-library-app.sh
@@ -22,10 +22,10 @@ sudo apt install maven -y
 git clone https://github.com/sumiya12m-bit/tech610-ubuntu.git
 
 # ── Set environment variables ──────────────────────────
-export DB_HOST=jdbc:mysql://172.31.61.89:3306/library
+export DB_HOST=jdbc:mysql://172.31.62.97:3306/library
 export DB_USER=admin
 export DB_PASS=password123
 
 # ── Run the app ────────────────────────────────────────
-cd /tech610-ubuntu/LibraryProject2
-mvn spring-boot:start
+cd ~/tech610-ubuntu/LibraryProject2
+mvn spring-boot:run
